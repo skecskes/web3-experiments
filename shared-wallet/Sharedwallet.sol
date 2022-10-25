17602e58cdcea7f4ec14735efa8447557ea1c9e6
stefan Kecskes <mr.kecskes@gmail.com>
2022-10-25 23:51:21 +0100

    add events

--- shared-wallet/Sharedwallet.sol
+++ shared-wallet/Sharedwallet.sol
@@ -4,11 +4,12 @@ pragma solidity 0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
-
+contract Allowance is Ownable {
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
     function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, _allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -18,8 +19,12 @@ contract SharedWallet is Ownable {
     }
 
     function reduceAllowance(address _who, uint amount) internal {
+        emit AllowanceChanged(_who, msg.sender, _allowance[_who], _allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
+}
+
+contract SharedWallet is Allowance {
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require (_amount <= address(this).balance, "There are not enough funds stored in the smart contract");
